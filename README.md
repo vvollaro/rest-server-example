b6aacff21034f52e56dbea22203b34ab927de0d1
Vincent VOLLARO <xxc86dino@gmail.com>
2018-12-05 18:16:27 +0100

    Creates the repository.

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is an example of a REST server written in python ready for a devops approach.
---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+from setuptools import find_packages, setup
+
+setup(
+    name = "rest-server-example",  # what you want to call the archive/egg
+    version = "0.0.1",
+    packages = find_packages('src'),  # top-level python modules you can import like
+    package_dir={'':'src'},  # set the directory where find the package
+    install_requires=['Flask==1.0.2'],
+    author="Vincent Vollaro",
+    description = "An example of program in Python",
+    keywords= "example tutorial",
+    url = "",
+)
